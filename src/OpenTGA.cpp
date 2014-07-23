96beb631ab71427b722d5d33a644aae17f529b50
Chad Austin <chad@chadaustin.me>
2014-07-22 21:42:22 -0700

    Add TGA/RLE support courtesy of Kevin Meinert

--- src/OpenTGA.cpp
+++ src/OpenTGA.cpp
@@ -8,6 +8,62 @@
 
 namespace corona {
 
+#define RLEBUFSIZ	512			// size of largest possible RLE packet 
+
+   // (this was fun to write.)
+   // data       - pointer to buffer to recieve uncompressed data.
+   // bufferSize - buffer size in bytes
+   // bpp        - bytes per pixel (8, 16, 24, 32)
+   // fp         - file pointer, must be an opened file, ::use fseek to position at valid start of RLE row.
+   int ReadRLERow( unsigned char* data, const int& bufferSize, const int& bpp, File* file )
+   {
+      unsigned int   value;
+      unsigned char   byte;   // for reading byte by byte.
+      int      i;
+      unsigned char*   q;
+      int      n( bufferSize );
+      static unsigned char rleBuf[RLEBUFSIZ];
+
+      while ( n > 0 )
+      {
+         if (file->read(&byte, 1) != 1) return -1;
+         value = byte;
+
+         if ( value & 0x80 )
+         {
+            value &= 0x7f;
+            value++;
+            n -= value * bpp;
+            if ( n < 0 )
+               return( -1 );
+            if (file->read( rleBuf, bpp ) != bpp) return( -1 );
+            while ( value > 0 )
+            {
+               *data++ = rleBuf[0];
+               if ( bpp > 1 ) *data++ = rleBuf[1];
+               if ( bpp > 2 ) *data++ = rleBuf[2];
+               if ( bpp > 3 ) *data++ = rleBuf[3];
+               value--;
+            }
+         }
+         else
+         {
+            value++;
+            n -= value * bpp;
+            if ( n < 0 )
+               return( -1 );
+
+            // Maximum for value is 128 so as long as RLEBUFSIZ
+            // is at least 512, and bpp is not greater than 4
+            // we can read in the entire raw packet with one operation.
+            if (file->read( rleBuf, bpp * value ) != (bpp * value) )
+               return( -1 );
+            for ( i = 0, q = rleBuf; i < (value * bpp); ++i ) *data++ = *q++;
+         }
+      }
+      return( 0 );
+   }
+
   Image* OpenTGA(File* file) {
     COR_GUARD("OpenTGA");
 
@@ -30,7 +86,7 @@ namespace corona {
     int height           = read16_le(header + 14);
     int pixel_depth      = header[16];
     int image_descriptor = header[17];
-    
+
     bool mirrored = (image_descriptor & (1 << 4)) != 0;  // left-to-right?
     bool flipped  = (image_descriptor & (1 << 5)) == 0;  // bottom-to-top?
 
@@ -46,8 +102,10 @@ namespace corona {
      */
 
     // make sure we support the image
-    if (image_type != 2 || (pixel_depth != 24 && pixel_depth != 32)) {
-      return 0;
+    if ((image_type != 2 && image_type != 10) ||
+        (pixel_depth != 24 && pixel_depth != 32)) {
+       COR_LOG( "wrong image_type (2 or 10) or pixel_depth (24 or 32)\n" );
+       return 0;
     }
 
     // skip image id
@@ -74,22 +132,58 @@ namespace corona {
 
       COR_LOG("24-bit image");
 
+      int bytesPerPixel = (pixel_depth/8);
       format = PF_B8G8R8;
-      int image_size = width * height * 3;
+      int image_size = width * height * bytesPerPixel;
       pixels = new byte[image_size];
-      if (file->read(pixels, image_size) != image_size) {
-        return 0;
+
+      if (image_type == 10)
+      {
+         int count = width * bytesPerPixel;
+         for (int i = 0; i < height; ++i)
+         {
+            // expand one scanline of RLE data from the file...
+            if (ReadRLERow( &pixels[i*width*bytesPerPixel], count, bytesPerPixel, file ) < 0)
+            {
+               COR_LOG( "Error decompressing RLE data.\n" );
+               return 0;
+            }
+         }
+      }
+      else if (image_type == 2)
+      {
+         if (file->read(pixels, image_size) != image_size) {
+         return 0;
+         }
       }
 
     } else if (pixel_depth == 32) {
 
       COR_LOG("32-bit image");
 
+      int bytesPerPixel = (pixel_depth/8);
       format = PF_B8G8R8A8;
-      int image_size = width * height * 4;
+      int image_size = width * height * bytesPerPixel;
       pixels = new byte[image_size];
-      if (file->read(pixels, image_size) != image_size) {
-        return 0;
+
+      if (image_type == 10)
+      {
+         int count = width * bytesPerPixel;
+         for (int i = 0; i < height; ++i)
+         {
+            // expand one scanline of RLE data from the file...
+            if (ReadRLERow( &pixels[i*width*bytesPerPixel], count, bytesPerPixel, file ) < 0)
+            {
+               COR_LOG( "Error decompressing RLE data.\n" );
+               return 0;
+            }
+         }
+      }
+      else if (image_type == 2)
+      {
+         if (file->read(pixels, image_size) != image_size) {
+         return 0;
+         }
       }
 
     } else {
